a9e20d9ed1d2a5d7f4f0cafd7e85c74887053bb7
umekaf <kafleumesh@gmail.com>
2022-06-13 23:11:50 +0200

    Added logic to handle in all note actions

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,22 +1,56 @@
 class NotesController < ApplicationController
+  before_action :set_note, only: %i[ show edit update destroy ]
+
   def index
+    @notes = Note.all
   end
 
   def show
   end
 
   def new
-  end
-
-  def create
+    @note = Note.new
   end
 
   def edit
   end
 
+  def create
+    @note = Note.new(note_params)
+
+    respond_to do |format|
+      if @note.save
+        format.html { redirect_to note_url(@note), notice: "Note was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def update
+    respond_to do |format|
+      if @note.update(note_params)
+        format.html { redirect_to note_url(@note), notice: "Note was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @note.destroy
+
+    respond_to do |format|
+      format.html { redirect_to notes_url, notice: "Note was successfully destroyed." }
+    end
   end
+
+  private
+    def set_note
+      @note = Note.find(params[:id])
+    end
+
+    def note_params
+      params.require(:note).permit(:title, :content)
+    end
 end
